6b1319222686cc10a5b7819958d70d2e177b0704
Mekan777-alt <mekan2678@gmail.com>
2024-05-29 20:18:01 +0300

    update navy

--- api/navy/routers.py
+++ api/navy/routers.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 from .company_info import router as company_info_router
-from .add_favorite import router as favorite_router
 
 company_router = APIRouter(
     prefix="/ap1/v1",
@@ -8,4 +7,3 @@ company_router = APIRouter(
 )
 
 company_router.include_router(company_info_router)
-company_router.include_router(favorite_router)
